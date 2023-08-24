36edc576be7442543bb76d25ac189398bfb5bd8e
Adel Alkhesh <adel.alkhesh@rumble.de>
2023-08-24 21:17:00 +0200

    Added: Some minor testing of class functionality in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
+#include "Archer.h"
+#include "Character.h"
+#include "Warrior.h"
 #include <iostream>
+
+using std::cout;
+using std::endl;
+
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  Archer archer("archer", 50, 30, 20);
+  Warrior warrior("warrior", 70, 30, 20);
+  Character dummy("dummy", 100, 40, 0);
+
+  archer.useAim();
+  archer.attack(warrior);
+  cout << warrior.getName() << " has: " << warrior.getHeath() << " health."
+       << endl;
+
+  warrior.useRoar();
+  warrior.attack(dummy);
+
+  cout << dummy.getName() << " has: " << dummy.getHeath() << " health." << endl;
+
   return 0;
 }
